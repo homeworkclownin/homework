29c33315620119004e41224e148f2f49daf8a936
homeworkclownin <57743651+homeworkclownin@users.noreply.github.com>
2019-11-24 03:57:41 +0000

    hey mom im not sketchy anymore

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+$("#dark-mode-toggle").click(() => {
+    // todo: find out if there is a better way to do this.
+    const table = document.getElementById("table");
+    const header = document.getElementById("header");
+    const toggle = document.getElementById("dark-mode-toggle");
+    const contact = document.getElementById("contact");
+    const body = document.body;
+
+    if(table.classList.contains("table-dark")){
+        table.classList.remove("table-dark");
+        header.classList.remove("dark");
+        toggle.classList.remove("dark");
+        contact.classList.remove("dark");
+        body.classList.remove("dark");
+        $('#contact h5').children('a').css('color', '#212529');
+    } else {
+        body.classList.add("dark");
+        setTimeout(() => {
+            table.classList.add("table-dark");
+            header.classList.add("dark");
+            toggle.classList.add("dark");
+            contact.classList.add('dark');
+            $('#contact h5').children('a').css('color', '#fff');
+        }, 60);
+    }
+})
